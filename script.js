a5ddf68d600d28c259a65a39991adae97b6fd105
Iain <iain@iainferguson.org>
2020-05-31 15:18:44 +0100

    Added additional functions to calculator: percentages, clear entry, plus/minus. Tidied up HTML and CSS.

--- script.js
+++ script.js
@@ -3,67 +3,102 @@ const innerDisplay = document.getElementById("display");
 
 let a = "";
 let b = "";
-let operator;
+let operator = "";
 
-function add(a,b) {
-    return a + b;
+function operate() {
+  switch (operator) {
+    case "add":
+      return a + b;
+    case "subtract":
+      return a - b;
+    case "multiply":
+      return Math.round((a * b + Number.EPSILON) * 100) / 100;
+    case "divide":
+      return Math.round((a / b + Number.EPSILON) * 100) / 100;
+  }
 }
-const subtract = (a,b) => a - b;
-const multiply = (a,b) => a * b;
-const divide = (a, b) => (Math.round((a/b) * 100) / 100).toFixed(2);
 
-function operate(operator, a, b) {
-    return operator(a,b);
-}
+document.onkeypress = function () {
+  if (event.key >= 0 && event.key <= 9) {
+    if (innerDisplay.innerHTML == 0) {
+      innerDisplay.innerHTML = "";
+    }
+    if (operator && !a) {
+      a = +innerDisplay.innerHTML;
+      innerDisplay.innerHTML = "";
+    }
+    innerDisplay.innerHTML += event.key;
+  }
+
+  if (event.key == "Enter" && a && operator) {
+    b = +innerDisplay.innerHTML;
+    innerDisplay.innerHTML = operate();
+    a = "";
+    b = "";
+  }
+};
 
 buttonPress.addEventListener("click", (event) => {
-    const isButton = event.target.nodeName === "BUTTON";
-    if (!isButton) {
-        return;
-    }
-    
-    const buttonValue = event.target.id;
-    const buttonClass = event.target.class;
-    console.dir(buttonValue);
+  const isButton = event.target.nodeName === "BUTTON";
+  if (!isButton) {
+    return;
+  }
 
-    if (buttonValue == "clear") {
-        innerDisplay.innerHTML = 0;
-        a = "";
-        b = "";
-        operator = "";
-    }   
+  const buttonValue = event.target.id;
 
-    if ((+buttonValue >= 0) && (+buttonValue <= 9)) {
-        if(innerDisplay.innerHTML == 0) {
-            innerDisplay.innerHTML = "";
-        }
-        if((operator) && !(a)){
-            a = +innerDisplay.innerHTML;
-            innerDisplay.innerHTML = ""
-        }
-        innerDisplay.innerHTML += buttonValue;
+  if (buttonValue == "clear") {
+    innerDisplay.innerHTML = 0;
+    a = "";
+    b = "";
+    operator = "";
+  }
+
+  if (buttonValue == "clearEntry") {
+    innerDisplay.innerHTML = 0;
+  }
+
+  if (buttonValue == "plusMinus") {
+    if (innerDisplay.innerHTML[0] == "-") {
+      innerDisplay.innerHTML = innerDisplay.innerHTML.substring(1);
+    } else {
+      innerDisplay.innerHTML = "-" + innerDisplay.innerHTML;
+      }
+  }
+
+  if (buttonValue >= 0 && buttonValue <= 9) {
+    if (innerDisplay.innerHTML == 0) {
+      innerDisplay.innerHTML = "";
+    }
+    if (operator && !a) {
+      a = +innerDisplay.innerHTML;
+      innerDisplay.innerHTML = "";
     }
 
-    if ((buttonValue == "decimal") && ! (innerDisplay.innerHTML.includes("."))) {
-        innerDisplay.innerHTML += event.target.innerHTML;
-    }
+    innerDisplay.innerHTML += buttonValue;
+  }
 
-    if (buttonValue == ("add" || "subtract" || "multiply" || "divide")) {
-        operator = buttonValue;
-    }
+  if (buttonValue == "decimal" && !innerDisplay.innerHTML.includes(".")) {
+    innerDisplay.innerHTML += event.target.innerHTML;
+  }
 
-    if (buttonValue == "equals" && (a) && (operator)) {
-        b = +innerDisplay.innerHTML;
-        if (operator == "add") {
-            innerDisplay.innerHTML = operate(add, a, b);
-        } else if (operator == "subtract") {
-            innerDisplay.innerHTML = operate(subtract, a, b);
-        } else if (operator == "multiply") {
-            innerDisplay.innerHTML = operate(multiply, a, b);
-        } else {
-            innerDisplay.innerHTML = operate(divide, a, b);
-        }
-            
+  if (buttonValue == "percent" && a) {
+    innerDisplay.innerHTML = (a / 100) * innerDisplay.innerHTML;
+  }
+
+  if (["add", "subtract", "multiply", "divide"].includes(buttonValue)) {
+    if (operator && a) {
+      b = +innerDisplay.innerHTML;
+      innerDisplay.innerHTML = operate();
+      a = "";
+      b = "";
     }
-        
-})
+    operator = buttonValue;
+  }
+
+  if (buttonValue == "equals" && a && operator) {
+    b = +innerDisplay.innerHTML;
+    innerDisplay.innerHTML = operate();
+    a = "";
+    b = "";
+  }
+});
